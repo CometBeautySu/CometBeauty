33a6140e43ff8e76e6a44989cd116501298f9c64
CometBeautySu <susu.sutianyao@gmail.com>
2023-09-13 16:20:22 -0700

    Added Image Slider

--- comet-beauty/src/App.js
+++ comet-beauty/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import GlobalStyle from './globalStyles';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './data/SliderData';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
+      <ImageSlider slides={SliderData}/>
     </>
   );
 }
---
+++ comet-beauty/src/data/SliderData.js
@@ -0,0 +1,76 @@
+import ImageOne from '../images/image1.jpg'
+import ImageTwo from '../images/image2.jpg'
+import ImageThree from '../images/image3.jpg'
+import ImageFour from '../images/image4.jpg'
+// import ImageOne from '../images/image6.jpg'
+
+
+export const SliderData = [
+    {
+        title: "Board",
+        price: "$40,000",
+        path: "/homes",
+        label: "View Home",
+        image: ImageOne,
+        alt: "house"
+    },
+    {
+        title: "Bread",
+        price: "$40,000",
+        path: "/homes",
+        label: "View Home",
+        image: ImageTwo,
+        alt: "house"
+    },
+    {
+        title: "Cut",
+        price: "$40,000",
+        path: "/homes",
+        label: "View Home",
+        image: ImageThree,
+        alt: "house"
+    },
+    {
+        title: "Banana",
+        price: "$40,000",
+        path: "/homes",
+        label: "View Home",
+        image: ImageFour,
+        alt: "house"
+    },
+];
+
+// export const SliderDataTwo = [
+//     {
+//         title: "A1",
+//         price: "$40,000",
+//         path: "/homes",
+//         label: "View Home",
+//         image: ImageFive,
+//         alt: "house"
+//     },
+//     {
+//         title: "A1",
+//         price: "$40,000",
+//         path: "/homes",
+//         label: "View Home",
+//         image: ImageSix,
+//         alt: "house"
+//     },
+//     {
+//         title: "A1",
+//         price: "$40,000",
+//         path: "/homes",
+//         label: "View Home",
+//         image: ImageSeven,
+//         alt: "house"
+//     },
+//     {
+//         title: "A1",
+//         price: "$40,000",
+//         path: "/homes",
+//         label: "View Home",
+//         image: ImageEight,
+//         alt: "house"
+//     },
+// ];
