e7bb546dfa6e753e7298fa8d9d651f96fcaca467
CometBeautySu <susu.sutianyao@gmail.com>
2023-09-14 11:09:46 -0700

    Added dropdown menu

--- comet-beauty/src/App.js
+++ comet-beauty/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import GlobalStyle from './globalStyles';
 import ImageSlider from './components/ImageSlider';
 import { SliderData } from './data/SliderData';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  // func for menu bar
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Navbar />
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
       <ImageSlider slides={SliderData}/>
     </>
   );
