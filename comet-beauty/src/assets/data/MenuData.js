97313f25c5ff280e7a4bb7ade3517dc0c9999e97
CometBeautySu <susu.sutianyao@gmail.com>
2024-05-28 17:16:55 -0700

    Update links, resolve same page refresh 404

--- comet-beauty/src/assets/data/MenuData.js
+++ comet-beauty/src/assets/data/MenuData.js
@@ -1,6 +1,6 @@
 export const menuData = [
   { title: 'Home', link: '/' },
   { title: 'Services', link: '/#services' },
-  { title: 'Gallery', link: '/gallery/#gallery' },
+  { title: 'Gallery', link: '/gallery' },
   // { title: 'Rentals', link: '/rentals' }
 ]
